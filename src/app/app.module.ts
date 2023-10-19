0449d1d78e124709af45d875a834f664bf7aae3a
Guilherme <g.sdomingues@hotmail.com>
2023-10-19 18:37:15 -0300

    Criacao do entrar e cadastrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { FillInComponent } from './fill-in/fill-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    SignInComponent,
+    FillInComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/fill-in/fill-in.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fill-in',
+  templateUrl: './fill-in.component.html',
+  styleUrls: ['./fill-in.component.css']
+})
+export class FillInComponent {
+
+}
