e7a2a67590819a33d9c90f73db417d9e2b155f10
Guilherme <g.sdomingues@hotmail.com>
2023-10-19 19:09:44 -0300

    Implementação da rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { FillInComponent } from './fill-in/fill-in.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'', redirectTo: 'sign-in', pathMatch: 'full'},
+  
+  {path: 'sign-in', component: SignInComponent},
+  {path:'menu', component: MenuComponent}, 
+  {path:'rodape', component: RodapeComponent}, 
+  {path: 'fill-in', component: FillInComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
